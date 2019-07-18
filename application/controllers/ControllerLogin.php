146f42d838386ed18edc983643b913664d5ab260
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-07-18 13:33:52 +0700

    Fix Login css

--- application/controllers/ControllerLogin.php
+++ application/controllers/ControllerLogin.php
@@ -6,7 +6,7 @@ class ControllerLogin extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->model("ModalLogin");
+
 	}
 	public function index(){
 		$this->load->view('ViewLogin');
--- application/views/ViewLogin.php
+++ application/views/ViewLogin.php
@@ -4,9 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="../../public/css/Theme.css">
-  <link rel="stylesheet" href="../../public/css/StyleLogin.css">
-  <title>Quản Lý Giám Sát Nhiễm Khuẩn</title>
+  <link rel="stylesheet" href=" <?php echo base_url('/public/css/Theme.css')?>">
+  <link rel="stylesheet" href="<?php echo base_url('/public/css/StyleLogin.css')?>">
   <script src="https://kit.fontawesome.com/d492333a0e.js"></script>
 </head>
 
@@ -41,7 +40,7 @@
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="../../public/js/ScriptLogin.js"></script>
+  <script src="<?php echo base_url('/public/js/ScriptLogin.js')?>"></script>
 </body>
 
 </html>
