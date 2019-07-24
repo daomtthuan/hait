2f405d0e2b4f70be49ed2591421fec2f41905648
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-07-24 21:03:32 +0700

    Alll

---
+++ application/controllers/Test.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends My_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		//Do your magic here
+	}
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file Controllername.php */
