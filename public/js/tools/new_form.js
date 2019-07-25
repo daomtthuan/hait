ed4166b14fad0907152c9c09bada33afaa792165
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2019-07-26 02:43:19 +0700

    update form

--- public/js/tools/new_form.js
+++ public/js/tools/new_form.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   var now = new Date();
+  var numberStep = 8;
 
   $("#ngay_dieu_tra").val(now.getFullYear() + "-" + (("0" + (now.getMonth() + 1)).slice(-2)) + "-" + (("0" + now.getDate()).slice(-2)));
 
@@ -31,17 +32,29 @@ $(document).ready(function () {
     else $("." + $(this).attr("name")).fadeOut(200);
   });
 
+  $("#step8 input[type='radio']").on("click", function () {
+    if ($(this).val() == "5") {
+      $("." + $(this).attr("name")).fadeIn(200);
+      $("." + $(this).attr("name") + " input").removeAttr("readonly");
+    }
+    else {
+      $("." + $(this).attr("name")).fadeOut(200);
+      $("." + $(this).attr("name") + " input").attr("readonly", "readonly").prop("checked", false);
+    }
+  });
+
   $("#buttonStepNext").click(function () {
-    var step = ($(".progress-form-step.active").attr("id")).slice(7);
+    var step = parseInt(($(".progress-form-step.active").attr("id")).slice(7));
     var isValid = true;
     $("#step" + step + " .invalid-feedback").each(function () {
       if ($(this).css("display") == "block") { isValid = false; return; }
     });
     if (isValid) {
-      if (step == "1") $("#buttonStepBack").fadeIn(200);
-      else if (step == "10") $(this).fadeOut(200, function () { $("#buttonSubmit").fadeIn(200) });
-      $("#step" + eval(step) + "").fadeOut(200);
-      $("#step" + eval(step + 1) + "").fadeIn(200);
+      if (step == 1) $("#buttonStepBack").fadeIn(200);
+      else if (step == numberStep-1) $(this).fadeOut(200, function () { $("#buttonSubmit").fadeIn(200) });
+      $("#step" + eval(step)).fadeOut(200);
+      $("#step" + eval(step + 1)).fadeIn(200);
+      $("#step" + eval(step + 1) + " input").attr("required", "required");
       $(".progress-form-step:eq(" + eval(step - 1) + ")").removeClass("active").addClass("complete");
       $(".progress-form-step:eq(" + eval(step) + ")").removeClass("disabled").addClass("active");
     }
@@ -49,11 +62,11 @@ $(document).ready(function () {
 
   $("#buttonStepBack").click(function () {
     var step = ($(".progress-form-step.active").attr("id")).slice(7);
-    if (step == "1") return;
-    else if (step == "2") $(this).fadeIn(200);
-    else if (step == "10") $("#buttonSubmit").fadeOut(200, function () { $("#buttonStepNext").fadeIn(200) });
-    $("#step" + eval(step) + "").fadeOut(200);
-    $("#step" + eval(step - 1) + "").fadeIn(200);
+    if (step == 1) return;
+    else if (step == 2) $(this).fadeIn(200);
+    else if (step == numberStep) $("#buttonSubmit").fadeOut(200, function () { $("#buttonStepNext").fadeIn(200) });
+    $("#step" + eval(step)).fadeOut(200);
+    $("#step" + eval(step - 1)).fadeIn(200);
     $(".progress-form-step:eq(" + eval(step - 2) + ")").addClass("active").removeClass("complete");
     $(".progress-form-step:eq(" + eval(step - 1) + ")").addClass("disabled").removeClass("active");
   });
