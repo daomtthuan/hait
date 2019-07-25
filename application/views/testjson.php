6c9b66015de1eb067504819d28ccf57cadf37f9d
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-07-25 22:33:57 +0700

    Update testjson.php

--- application/views/testjson.php
+++ application/views/testjson.php
@@ -1,43 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
+	<title>Angular JS Includes</title>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>SB Admin 2 - Blank</title>
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <link href="css/theme.min.css" rel="stylesheet">
-
+	<style>
+		table, th , td {
+			border: 1px solid grey;
+			border-collapse: collapse;
+			padding: 5px;
+		}
+		table tr:nth-child(odd) {
+			background-color: #f2f2f2;
+		}
+		table tr:nth-child(even) {
+			background-color: #ffffff;
+		}
+	</style>
 </head>
 
-<body id="page-top">
+<body>
+<h2>AngularJS Sample Application</h2>
+<div ng-app = "" ng-controller = "studentController">
 
+	<table>
+		<tr>
+			<th>Name</th>
+			<th>Roll No</th>
+		</tr>
 
+		<tr ng-repeat = "student in students">
+			<td>{{ student.attribute_name }}</td>
+			<td>{{ student.value }}</td>
+		</tr>
+	</table>
+</div>
 
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="js/theme.min.js"></script>
+<script>
+	function studentController($scope,$http) {
+		var url = "http://localhost:8080/hait/api/get_value";
 
-  <script>
-    $.getJSON("ajax/test", function (data) {
-      var items = [];
-      $.each(data, function (key, val) {
-        items.push("<li id='" + key + "'>" + val + "</li>");
-      });
+		$http.get(url).then( function(response) {
+			$scope.students = response.data;
+		});
+	}
+</script>
 
-      $("<ul/>", {
-        "class": "my-new-list",
-        html: items.join("")
-      }).appendTo("body");
-    });
-  </script>
+<script src = "https://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js">
+</script>
+
 </body>
-
 </html>
