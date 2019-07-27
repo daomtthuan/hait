02b5c032f2c26404f560e439380c615e603965d8
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2019-07-27 15:45:13 +0700

    update

--- public/js/app.js
+++
@@ -1,7 +0,0 @@
-var databaseApp = angular.module("databaseApp", []);
-databaseApp.controller("databaseController", function($scope,$http) {
-	var url = "<?php echo base_url('test/ajax') ?>";
-	$http.get(url).then( function(response) {
-		$scope.students = response.data;
-	});
-});
