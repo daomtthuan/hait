4c1a91adb9d847dedac8b622b8251452af1715e6
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-07-28 15:32:41 +0700

    dsadsadsa

--- application/models/Value_model.php
+++ application/models/Value_model.php
@@ -7,8 +7,36 @@ class Value_model extends CI_Model
 	{
 		parent::__construct();
 	}
-	public function add($data){
-		$this->db->insert('value',$data);
+	public function insert_array( $batch){
+		$this->db->insert_batch('value', $batch);
+	}
+	public function insert($source_data,$form_id,$form_part){
+		$query1 = $this->db->get('view_datatype');
+		$batch=array();
+		foreach($source_data as $key => $val)
+		{
+			$datatype="";
+			$form_attribute_id=$form_id."_".$key;
+			foreach ($query1->result() as $row)
+			{
+				if($key==$row->attribute_name)
+				{
+					$datatype=$row->data_type;
+				}
+			}
+			if($val && $datatype){
+				$data=array(
+					"form_attribute_id"=>$form_attribute_id,
+					"form_id"=>$form_id,
+					"form_part"=>$form_part,
+					"attribute_name"=>$key,
+					"data_type"=>$datatype,
+					"value"=>$val
+				);
+				$batch[]= $data;
+			}
+		}
+		$this->db->insert_batch('value', $batch);
 	}
 	public function get(){
 		$query = $this->db->get('value');
@@ -28,6 +56,20 @@ class Value_model extends CI_Model
 	public function delete($id){
 		$this->db->where('form_attribute_id', $id);
 		$this->db->delete('value');
+	}
+	//helper
+	public function search($attribute_name){
+		$call_procedure ="call search_datatype('$attribute_name')";
+		$query=$this->db->query($call_procedure);
+		$row = $query->row();
+		if (isset($row))
+		{
+			return $row->data_type;
+
+		}
+	}
+	public function update_datatype($form_id){
+
 	}
 }
 
