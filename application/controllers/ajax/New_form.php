f8b91f476c22e436f38a6ccc4ac9fc7fc8b150ca
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-07-31 08:26:07 +0700

    new form

--- application/controllers/ajax/New_form.php
+++ application/controllers/ajax/New_form.php
@@ -7,14 +7,43 @@ class New_form extends REST_Controller
 	public function __construct($config = 'rest')
 	{
 		parent::__construct($config);
-		$this->load->model('Template_model');
+		$this->load->model('Value_model');
+		$this->load->model('Form_model');
 	}
 	function index_post()
 	{
-		if ($this->post()) {
-			$this->response(array('status' => 'success'));
-		} else {
+		$form_id=$this->Form_model->insert_new_form(1,'Khoa noi',1);
+		$data = $this->post(NULL,TRUE);
+		$query1 = $this->db->get('view_datatype');
+		$batch=array();
+		foreach($data as $key => $val)
+		{
+			$datatype="";
+			$form_attribute_id=$form_id."_".$key;
+			foreach ($query1->result() as $row)
+			{
+				if($key==$row->attribute_name)
+				{
+					$datatype=$row->data_type;
+				}
+			}
+			if($val && $datatype){
+				$data_item=array(
+					"form_attribute_id"=>$form_attribute_id,
+					"form_id"=>$form_id,
+					"form_part"=>"1",
+					"attribute_name"=>$key,
+					"data_type"=>$datatype,
+					"value"=>$val
+				);
+				$batch[]= $data_item;
+			}
+		}
+		$result=$this->db->insert_batch('value', $batch);
+		if ($result === FALSE) {
 			$this->response(array('status' => 'failed'));
+		} else {
+			$this->response(array('status' => 'success'));
 		}
 	}
 
