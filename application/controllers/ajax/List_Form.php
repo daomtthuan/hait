61e49ed7bb4d6f8a78271469416bd174a9e697b0
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-08-13 17:47:51 +0700

    Update List_Form.php

--- application/controllers/ajax/List_Form.php
+++ application/controllers/ajax/List_Form.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 require(APPPATH . '/libraries/REST_Controller.php');
-class Form_List extends REST_Controller
+class List_Form extends REST_Controller
 {
 
 	public function __construct($config = 'rest')
@@ -9,7 +9,6 @@ class Form_List extends REST_Controller
 		parent::__construct($config);
 		$this->load->model('Template_model');
 	}
-
 	function index_get()
 	{
 		$this->load->model('Form_model');
