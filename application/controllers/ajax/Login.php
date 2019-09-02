455ba85d38877f9a629e81a868303bc8fc766bdc
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-09-02 09:42:29 +0700

    Create Login.php
    Api:http://localhost:8080/hait/ajax/login
    Method: post
    Parameter:
    mail:
    password:
    remember: true/false

---
+++ application/controllers/ajax/Login.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require(APPPATH . '/libraries/REST_Controller.php');
+class Login extends REST_Controller
+{
+
+	public function __construct($config = 'rest')
+	{
+		parent::__construct($config);
+		$this->load->library('ion_auth');
+	}
+	function index_post()
+	{
+		$data = $this->post(NULL, TRUE);
+		$identity = $data['mail'];
+		$password = $data['password'];
+		$remember = $data['remember']; // remember the user
+		$result= $this->ion_auth->login($identity, $password, $remember);
+		log_message("debug","user: ".$identity." has been login ".$result);
+		if ($result) {
+			$this->response($result, REST_Controller::HTTP_OK);
+		} else {
+			$this->response(NULL, REST_Controller::HTTP_NOT_FOUND);
+		}
+	}
+}
