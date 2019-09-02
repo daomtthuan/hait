4ad7481db77459b6f0f63d0706466ec60ef668ff
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2019-09-02 19:15:19 +0700

    update

--- application/views/general/layout.php
+++ application/views/general/layout.php
@@ -103,7 +103,7 @@
 						<li class="nav-item dropdown no-arrow">
 							<a class="nav-link dropdown-toggle" href="" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 								<span class="mr-2 d-none d-lg-inline text-gray-600 small text-primary">
-									<?php echo $this->session->userdata('login');?>
+									User
 									<img class="img-profile rounded-circle" src="" alt="">
 							</a>
 							<div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
