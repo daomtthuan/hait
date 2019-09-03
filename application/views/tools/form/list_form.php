6db18c93af711244ad278cb0bc57a8e4e7753c18
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2019-09-03 18:46:04 +0700

    Update list_form.php

--- application/views/tools/form/list_form.php
+++ application/views/tools/form/list_form.php
@@ -25,7 +25,6 @@
           <th scope="col">Mã số bệnh án</th>
           <th scope="col">Họ tên bệnh nhân</th>
           <th scope="col">Năm sinh</th>
-          <th scope="col">Chuẩn đoán xác định</th>
           <th scope="col">Thao tác</th>
         </tr>
       </thead>
