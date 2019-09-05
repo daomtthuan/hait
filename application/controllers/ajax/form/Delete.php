177a20e0afd2adf23ab5ece3990a3c2afdc0eed2
NGUYỄN PHÚC DUY <38091334+DuaDeoLA@users.noreply.github.com>
2019-09-05 18:10:14 +0700

    Update Delete.php

--- application/controllers/ajax/form/Delete.php
+++ application/controllers/ajax/form/Delete.php
@@ -8,10 +8,14 @@ class Delete extends REST_Controller
 	{
 		parent::__construct($config);
 		$this->load->model('Form_model');
+        $this->load->model('Meta_link_model', 'meta');
+        $this->load->model('Value_model');
 	}
 	function index_get($para)
 	{
 		$this->Form_model->delete($para);
+        $this->Value_model->delete($para);
+        $this->meta->delete($para);
 		$this->response("success", REST_Controller::HTTP_OK);
 	}
 }
