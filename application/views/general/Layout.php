f6a612db23fde10a630352225f0bb9e6cb8ae195
Dao Minh Trung Thuan <47786892+daomtthuan@users.noreply.github.com>
2019-09-07 00:58:00 +0700

    Update Layout.php

--- application/views/general/Layout.php
+++ application/views/general/Layout.php
@@ -34,7 +34,7 @@
 				</div>
 			</li>
 			<hr class="sidebar-divider my-0">
-			<?php $this->load->view($role.'/sidebar') ?>
+			<?php $this->load->view($role.'/Sidebar') ?>
 			<hr class="sidebar-divider d-none d-md-block">
 			<div class="text-center d-none d-md-inline">
 				<button class="rounded-circle border-0" id="sidebarToggle"></button>
